3a370d1ab73dcc9c0c7afd9c2c7cd23ca7383efa
frankappolonia <fappolonia@gmail.com>
2022-05-20 15:08:57 -0400

    export for data functions

---
+++ data/index.js
@@ -0,0 +1,6 @@
+const inventoryFuncs = require("./inventory");
+
+/**This file exports the databse functions so they can be used in other modules */
+module.exports = {
+  inventoryFuncs: inventoryFuncs
+};
