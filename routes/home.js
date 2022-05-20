858e55c1f38e32b72b3b8ba2d94e5cb39594d9a9
frankappolonia <fappolonia@gmail.com>
2022-05-20 15:26:15 -0400

    intital route for specific items in inventory and template

--- routes/home.js
+++ routes/home.js
@@ -6,13 +6,24 @@ const inventoryFuncs = db.inventoryFuncs;
 const validations = errorHandling.validations;
 const xss = require('xss');
 
-router.route("/")
+router.route("/") //this is the main route for the application landing page
     .get(async (request, response) => {
         try {
-        response.status(200).render("pages/home");
+            response.status(200).render("pages/home");
         } catch (e) {
-        console.log(e);
-        response.status(404).render("errors/404");
-        }
+            console.log(e);
+            response.status(404).render("errors/404");
+        };
     });
+
+router.route("/:id") //this is the route for specific items in the inventory
+    .get(async (request, response) => {
+        try {
+            response.status(200).render("pages/itemPage");
+        } catch (e) {
+            console.log(e);
+            response.status(404).render("errors/404");
+        };
+
+    })
   module.exports = router;
